23a98dc59757e5ec2cae79c539b8c4549b0629cd
queenelsamd <mywhatsappbot79@gmail.com>
2025-02-26 00:44:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID|| "WfglxL6Z#b0eantiFdPr2_jBeb_ju7ibjdITmwGr5WWP7vC_1LQA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/queenelsamd/Queen-Elsa-help-/blob/main/Vmake1740248561141%20(1).png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "👋 𝗵𝗲𝗹𝗹𝗼, 𝗶𝗺 𝗤𝘂𝗲𝗲𝗻 𝗘𝗹𝘀𝗮 𝗶𝗺 𝗮𝗹𝗶𝘃𝗲 𝗻𝗼𝘄",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true".
 };
