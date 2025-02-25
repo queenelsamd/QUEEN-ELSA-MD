858494a295e00af977ae92e3a4282852c8ee6b76
queenelsamd <mywhatsappbot79@gmail.com>
2025-02-25 19:50:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "",
+SESSION_ID: process.env.SESSION_ID|| "WfglxL6Z#b0eantiFdPr2_jBeb_ju7ibjdITmwGr5WWP7vC_1LQA",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/queenelsamd/Queen-Elsa-help-/blob/main/Vmake1740248561141%20(1).png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "👋 𝗵𝗲𝗹𝗹𝗼, 𝗶𝗺 𝗤𝘂𝗲𝗲𝗻 𝗘𝗹𝘀𝗮 𝗶𝗺 𝗮𝗹𝗶𝘃𝗲 𝗻𝗼𝘄",
 };
