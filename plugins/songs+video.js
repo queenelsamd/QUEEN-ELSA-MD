3c609784455a91ba50e0ecba11388b656f86c174
queenelsamd <mywhatsappbot79@gmail.com>
2025-03-05 10:58:45 +0530

    Update songs+video.js

--- plugins/songs+video.js
+++ plugins/songs+video.js
@@ -1,7 +1,6 @@
-const config = require('../config')
-const fg = require('api-dylux')
-const yts = require('yt-search')
-
+const config = require('../config');
+const fg = require('api-dylux');
+const yts = require('yt-search');
 
 cmd({
     pattern: "song",
@@ -9,14 +8,15 @@ cmd({
     category: "download",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if (!q) return reply("𝗽𝗹𝗲𝗮𝘀𝗲 𝗴𝗶𝘃𝗲 𝗺𝗲 𝘂𝗿𝗹 & 𝗻𝗮𝗺𝗲"
-const search = await yts(q)
-const deta = search.videos [0];
-const url =deta.url
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q) return reply("𝗽𝗹𝗲𝗮𝘀𝗲 𝗴𝗶𝘃𝗲 𝗺𝗲 𝘂𝗿𝗹 & 𝗻𝗮𝗺𝗲");
+        
+        const search = await yts(q);
+        const deta = search.videos[0];
+        const url = deta.url;
 
-les desc = `
+        let desc = `
 🎵 𝗤𝗨𝗘𝗘𝗡 𝗘𝗟𝗦𝗔 𝗦𝗢𝗡𝗚 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥 🎵
 
 📜 title: ${deta.title}
@@ -30,25 +30,21 @@ les desc = `
 🚀 views: ${deta.views}
 
 💙𝗠𝗔𝗗𝗘 𝗕𝗬 𝗤𝗨𝗘𝗘𝗡 𝗘𝗟𝗦𝗔 𝗠𝗗💙
-`
-await conn.sendmessage(from,{image:{url: deta,thumbanil},caption:deas},{quoted:mek});
+`;
+        await conn.sendMessage(from, { image: { url: deta.thumbnail }, caption: desc }, { quoted: mek });
 
- //=======download-audio+document==========
+        //=======download-audio+document==========
+        let down = await fg.yta(url);
+        let downloadUrl = down.dl_url;
 
-let down = await fg.yta(url)
-let downloadUrl = down.dl_url
-
-// send audio
-await conn.sendmessage(audio: {url:downloadUrl},mimemype;"audio/mpeg"},{quoted:mek});
-await conn.sendmessage(document: {url:downloadUrl},mimemype;"document/mpeg",fileName:deta.title + ".mp4"},caption:"💙𝗠𝗔𝗗𝗘 𝗕𝗬 𝗤𝗨𝗘𝗘𝗡 𝗘𝗟𝗦𝗔 𝗠𝗗💙"{quoted:mek});
-
-  
-}catch(e){
-concole.log(e)
-reply('${e}' )
-}
-})
-
+        // send audio
+        await conn.sendMessage(from, { audio: { url: downloadUrl }, mimetype: "audio/mpeg" }, { quoted: mek });
+        await conn.sendMessage(from, { document: { url: downloadUrl }, mimetype: "audio/mpeg", fileName: deta.title + ".mp3" }, { caption: "💙𝗠𝗔𝗗𝗘 𝗕𝗬 𝗤𝗨𝗘𝗘𝗡 𝗘𝗟𝗦𝗔 𝗠𝗗💙", quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
 
 //============video-dl=============
 cmd({
@@ -57,14 +53,15 @@ cmd({
     category: "download",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if (!q) return reply("𝗽𝗹𝗲𝗮𝘀𝗲 𝗴𝗶𝘃𝗲 𝗺𝗲 𝘂𝗿𝗹 & 𝗻𝗮𝗺𝗲"
-const search = await yts(q)
-const deta = search.videos [0];
-const url =deta.url
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q) return reply("𝗽𝗹𝗲𝗮𝘀𝗲 𝗴𝗶𝘃𝗲 𝗺𝗲 𝘂𝗿𝗹 & 𝗻𝗮𝗺𝗲");
+        
+        const search = await yts(q);
+        const deta = search.videos[0];
+        const url = deta.url;
 
-les desc = `
+        let desc = `
 🎵 𝗤𝗨𝗘𝗘𝗡 𝗘𝗟𝗦𝗔 𝗩𝗜𝗗𝗘𝗢 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥 🎵
 
 📜 title: ${deta.title}
@@ -78,21 +75,18 @@ les desc = `
 🚀 views: ${deta.views}
 
 💙𝗠𝗔𝗗𝗘 𝗕𝗬 𝗤𝗨𝗘𝗘𝗡 𝗘𝗟𝗦𝗔 𝗠𝗗💙
-`
-await conn.sendmessage(from,{image:{url: deta,thumbanil},caption:deas},{quoted:mek});
+`;
+        await conn.sendMessage(from, { image: { url: deta.thumbnail }, caption: desc }, { quoted: mek });
 
- //=======download-video+document========
+        //=======download-video+document========
+        let down = await fg.ytv(url);
+        let downloadUrl = down.dl_url;
 
-let down = await fg.ytv(url)
-let downloadUrl = down.dl_url
-
-// send video
-await conn.sendmessage(video: {url:downloadUrl},mimemype;"video/mp4"},{quoted:mek});
-await conn.sendmessage(document: {url:downloadUrl},mimemype;"video/mp4",fileName:deta.title + ".mp4"},caption:"💙𝗠𝗔𝗗𝗘 𝗕𝗬 𝗤𝗨𝗘𝗘𝗡 𝗘𝗟𝗦𝗔 𝗠𝗗💙"{quoted:mek});
-
-  
-}catch(e){
-concole.log(e)
-reply('${e}' )
-}
-})
+        // send video
+        await conn.sendMessage(from, { video: { url: downloadUrl }, mimetype: "video/mp4" }, { quoted: mek });
+        await conn.sendMessage(from, { document: { url: downloadUrl }, mimetype: "video/mp4", fileName: deta.title + ".mp4" }, { caption: "💙𝗠𝗔𝗗𝗘 𝗕𝗬 𝗤𝗨𝗘𝗘𝗡 𝗘𝗟𝗦𝗔 𝗠𝗗💙", quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
